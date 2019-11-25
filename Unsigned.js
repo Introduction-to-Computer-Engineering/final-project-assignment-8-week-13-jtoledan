7e8551e54f126baf123b862b2f624b9ceaa2e7a1
jtoledan <54724851+jtoledan@users.noreply.github.com>
2019-11-24 21:08:47 -0700

    Create Unsigned.js

---
+++ Unsigned.js
@@ -0,0 +1,4 @@
+basic.forever(function () {//runs continuously
+    pins.i2cWriteNumber(0x1E, 2055, NumberFormat.UInt8LE)// writes to 0x1E
+    basic.pause(100); //pause to so the user can see what is going on
+}) 
